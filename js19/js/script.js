a3baa27b97d4b4724c54ed5d6e6bd696259b3c17
gasakiri <morisakirita@tutamail.com>
2025-08-27 10:48:18 -0300

    Lesson 19

---
+++ js19/js/script.js
@@ -0,0 +1,24 @@
+/* JSON: JavaScript Object Notation */
+
+const myObj = {
+    name: "Gabriel",
+    hobbies: ["eat", "sleep", "code"],
+    hello: () => {
+        console.log("Hello!");
+    }
+}
+
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+console.log(sendJSON.name);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
+console.log(receiveJSON.name);
